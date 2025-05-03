af7460a175002e6f0659d560d4fe0d0629b136ff
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-05-03 14:39:15 -0300

    refactor

--- fuel_gauge/fuel.py
+++ fuel_gauge/fuel.py
@@ -1,24 +1,29 @@
 def main():
+    x, y = get_fraction()
+    percentage = round(x / y * 100)
+    if percentage <= 1:
+        print("E")
+    elif percentage >= 99:
+        print("F")
+    else:
+        print(f"{percentage}%")
+
+
+def get_fraction():
     while True:
         try:
             user_input = input("Fraction: ")
-            x, y = user_input.split('/')
+            x, y = user_input.split("/")
             x = int(x)
             y = int(y)
-            if x > y:
-                raise ValueError
             if y == 0:
                 raise ZeroDivisionError
-            percentage = round(x / y * 100)
-            if percentage <= 1:
-                print("E")
-            elif percentage >= 99:
-                print("F")
-            else:
-                print(f"{percentage}%")
-            break
+            if x > y:
+                raise ValueError
+            return x, y
         except (ValueError, ZeroDivisionError):
             continue
+
 
 if __name__ == "__main__":
     main()
