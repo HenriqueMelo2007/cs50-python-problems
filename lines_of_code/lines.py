ff279edc56aedf64773af1ca2fe43b0cb67d5a40
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-05-17 14:29:18 -0300

    feat: one more exercise

--- lines_of_code/lines.py
+++ lines_of_code/lines.py
@@ -1,5 +1,20 @@
+import sys
+
+
 def main():
-  print("Hello, world")
+    if len(sys.argv) != 2:
+        sys.exit("Invalid number of arguments (2 arguments expected)")
+    user_file = sys.argv[1]
+    if not user_file.endswith(".py"):
+        sys.exit("Invalid file extesion (.py required)")
+    with open(user_file, "r") as python_file:
+        count = 0
+        for line in python_file:
+            if line.strip() == "" or line.strip().startswith("#"):
+                continue
+            count = count + 1
+        print(count)
+
 
 if __name__ == "__main__":
-  main()
+    main()
